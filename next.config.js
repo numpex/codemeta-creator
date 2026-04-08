403456844ff0368f0eff6d27bbacf4c1b9af02bd
Nathalie Furmento <nathalie.furmento@labri.fr>
2026-04-08 13:45:49 +0200

    fix basepath

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
-    basePath: '',
+    basePath: 'codemeta-creator',
   modularizeImports: {
     '@mui/icons-material': {
       transform: '@mui/icons-material/{{member}}',
